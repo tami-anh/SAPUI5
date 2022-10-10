44906d90b925c72d25d5aeb4704dc80e78664b8a
Tami <tami.kyanh@gmail.com>
2022-10-10 16:07:55 +0200

    Moving the app assets to the component

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,30 +3,19 @@ sap.ui.define([
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel"
-], function(Controller, MessageToast, JSONModel, ResourceModel){
+], function(Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App", {
         onInit: function() {
-            const oController = this;
-            const oData = {
-                recipient: {
-                    name: "Tami"
-                }
-            };
-            const oModel = new JSONModel(oData);
-            oController.getView().setModel(oModel);
-
-            const i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            oController.getView().setModel(i18nModel, "i18n");
-
+            
         },
 
         onShowHelloPress: function() {
-            MessageToast.show("Hello there!");
+            const oController = this;
+            const oBundle = oController.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = oController.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            MessageToast.show(sMsg);
         }
     })
 })
