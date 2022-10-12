597a8dc3f6379e27fb5b9ecbd86a12aafc83098d
Tami <tami.kyanh@gmail.com>
2022-10-12 12:42:30 +0200

    Add manifest file

--- webapp/component.js
+++ webapp/component.js
@@ -6,12 +6,7 @@ sap.ui.define([
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata: {
-            rootView: {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                "async": true,
-                "id": "app"
-            }
+            manifest: "json"
         },
         init: function() {
             //call the init function of the parent
@@ -25,14 +20,6 @@ sap.ui.define([
             };
             const oModel = new JSONModel(oData);
             this.setModel(oModel);
-
-            //set the i18n model
-            const i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            this.setModel(i18nModel, "i18n");
         }
     })
 })
