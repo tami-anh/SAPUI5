0760e96ee1796f2e8deaaddc47dcaa5683676c01
Tami <tami.kyanh@gmail.com>
2022-10-17 13:52:10 +0200

    Adding a dialog

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function(Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+], function(Controller, MessageToast, Fragment) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App", {
         onInit: function() {
@@ -14,6 +15,31 @@ sap.ui.define([
             const sRecipient = oController.getView().getModel().getProperty("/recipient/name");
             const sMsg = oBundle.getText("helloMsg", [sRecipient]);
             MessageToast.show(sMsg);
+        },
+
+        onOpenDialog: function() {
+            const oController = this;
+            const oView = oController.getView()
+
+            //create the dialog lazily
+            if(!oController.byId("helloDialog")) {
+                //load asynchronous XML fragment
+                Fragment.load({
+                    id: oView.getId(""),
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                    controller: oController
+                }).then(function(oDialog) {
+                    //connect dialog to the root view of this component (models, lifecycle)
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                });
+            } else {
+                oController.byId("helloDialog").open();
+            }
+        },
+
+        onCloseDialog: function() {
+            this.byId("helloDialog").close();
         }
     })
 })
