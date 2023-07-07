0fe0a0539997cf3a9a78eb686bb73fc239dc6479
tami-anh <tami.kyanh@gmail.com>
2023-07-07 13:29:52 +0200

    Adding unit tests

--- webapp/test/initMockServer.js
+++ webapp/test/initMockServer.js
@@ -6,6 +6,6 @@ sap.ui.define([
     // initialise the mock server
     mockserver.init();
 
-    // initialise the embedded component o nthe HTML page
-    sap.ui.require(["sap/ui/core/ComponentSupport"])
+    // initialise the embedded component on the HTML page
+    sap.ui.require(["sap/ui/core/ComponentSupport"]);
 })
