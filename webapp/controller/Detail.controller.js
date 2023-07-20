5c28de430b9efa0e752318fe015c1a25a0ec8865
tami-anh <tami.kyanh@gmail.com>
2023-07-20 11:28:42 +0200

    Adding custom control

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
-    "sap/ui/core/routing/History"
-], function(Controller, UIComponent, History) {
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast"
+], function(Controller, UIComponent, History, MessageToast) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.Detail", {
         onInit: function() {
@@ -11,6 +12,7 @@ sap.ui.define([
         },
 
         _onObjectMatched: function(oEvent) {
+            this.byId("rating").reset();
             this.getView().bindElement({
                 path: "/"  + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "invoice"
@@ -27,7 +29,14 @@ sap.ui.define([
                 const oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("overview", {}, true);
             }
-        }
+        },
+
+        onRatingChange: function(oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+		}
 
     })
 })
